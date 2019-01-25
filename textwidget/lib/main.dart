24457e5aa1738c1905f304e1cced672834f5ee26
hcw <897796171@qq.com>
2019-01-25 16:01:43 +0800

    'textwidget学习'

---
+++ textwidget/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text widget',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'hello jerry， 非常喜欢技术,你没想到吧，哈哈哈我就是测试一下控件 好用不哈哈哈啊哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+            textAlign: TextAlign.left,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color.fromARGB(255, 255, 125, 125),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
