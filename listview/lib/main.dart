3ba14b9f42b0405646f32f08b1525ae3b81e214b
hcw <897796171@qq.com>
2019-01-28 16:24:31 +0800

    动态listview

--- listview/lib/main.dart
+++ listview/lib/main.dart
@@ -1,22 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MyApp(items: new List<String>.generate(1000, (i) => 'Item $i')));
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+
+  MyApp({Key key, @required this.items}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'jerry Flutter Demo',
       home: Scaffold(
-        appBar: new AppBar(title: new Text("ListView Widget")),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-
-            )
-          ],
+        appBar: new AppBar(title: new Text('ListView Widget')),
+        body: new ListView.builder(
+          itemBuilder: (context, index) {
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          },
+          itemCount: items.length,
         ),
       ),
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+          width: 180.0,
+          color: Colors.lightBlue,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.amber,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.deepOrange,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.deepOrangeAccent,
+        )
+      ],
     );
   }
 }
