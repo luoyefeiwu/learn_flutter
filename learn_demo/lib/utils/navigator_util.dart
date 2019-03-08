4b7f4f9a2b8f7513f3e61a7d49d1d187db267936
hcw <897796171@qq.com>
2019-03-08 18:27:00 +0800

    学习 flutter

---
+++ learn_demo/lib/utils/navigator_util.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:learn_demo/common/component_index.dart';
+import 'package:learn_demo/ui/pages/tab_page.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+class NavigatorUtil{
+
+
+}
