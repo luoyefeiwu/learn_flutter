1f7400c7cbba3746e0160261b917ae26e537b4d2
hcw <897796171@qq.com>
2019-08-15 17:21:18 +0800

    容器

--- container/lib/main.dart
+++ container/lib/main.dart
@@ -3,47 +3,85 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            child: new Text(
-              "hello jerry",
-              style: TextStyle(fontSize: 40.0),
-            ),
-            alignment: Alignment.topLeft,
-            width: 500.0,
-            height: 400.0,
-            // color: Colors.lightBlue,
-            padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
-            margin: const EdgeInsets.all(10),
+      title: "Container 容器例子",
+      home: new Layoutdemo(),
+    );
+  }
+}
+
+class Layoutdemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //返回一个 Container 对象
+    Widget container = new Container(
+      //添加装饰效果
+      decoration: new BoxDecoration(color: Colors.grey),
+      //子元素指定一个垂直水平嵌套布局的组件
+      child: new Column(
+        children: <Widget>[
+          //使用 Expanded防止内存溢出
+          new Expanded(
+              child: new Container(
+            width: 150.0,
+            height: 150.0,
             decoration: new BoxDecoration(
-              gradient: const LinearGradient(colors: [
-                Colors.lightBlue,
-                Colors.greenAccent,
-                Colors.purple
-              ]),
-              border: Border.all(width: 5.0, color: Colors.red),
-            ),
-          ),
-        ),
+                //上下左右边框设置为宽度10.0，颜色为蓝灰色
+                border: new Border.all(width: 10.0, color: Colors.blueGrey),
+                //上下左右边框弧度设置为 8.0
+                borderRadius:
+                    const BorderRadius.all(const Radius.circular(8.0))),
+            //上下左右增加边框距
+            margin: const EdgeInsets.all(4.0),
+            //添加图片
+            child: new Image.asset("images/1.png"),
+          )),
+          new Expanded(
+              child: new Container(
+            width: 150.0,
+            height: 150.0,
+            decoration: new BoxDecoration(),
+            //上下左右增加边框距
+            margin: const EdgeInsets.all(4.0),
+            //添加图片
+            child: new Image.asset("images/2.jpg"),
+          )),
+          new Expanded(
+              child: new Container(
+            width: 150.0,
+            height: 150.0,
+            decoration: new BoxDecoration(
+                border: new Border.all(width: 10.0, color: Colors.blueGrey),
+                borderRadius:
+                    const BorderRadius.all(const Radius.circular(8.0))),
+            //上下左右增加边框距
+            margin: const EdgeInsets.all(4.0),
+            //添加图片
+            child: new Image.asset("images/3.jpg"),
+          )),
+          new Expanded(
+              child: new Container(
+            width: 150.0,
+            height: 150.0,
+            decoration: new BoxDecoration(
+                border: new Border.all(width: 10.0, color: Colors.blueGrey),
+                borderRadius:
+                    const BorderRadius.all(const Radius.circular(8.0))),
+            //上下左右增加边框距
+            margin: const EdgeInsets.all(4.0),
+            //添加图片
+            child: new Image.asset("images/4.jpg"),
+          ))
+        ],
       ),
+    );
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Container布局容器例子"),
+      ),
+      body: container,
     );
   }
 }
