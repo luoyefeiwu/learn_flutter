5e15a3d7d055709a17b57686fc8238eaea3308cb
hcw <897796171@qq.com>
2019-08-23 18:40:35 +0800

    基础 组件

--- flutter_im/lib/app.dart
+++ flutter_im/lib/app.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget{
+import 'chat/message_page.dart';
+
+//应用页面使用有状态 Widget
+class App extends StatefulWidget {
+  @override
+  AppState createState() => AppState();
+}
+
+//应用页面状态实现类
+class AppState extends State<App> {
+
+  //当前选中页面索引
+  var _currentIndex=0;
+
+  //聊天页面
+  MessagePage message;
+
+  //好友页面
+
+  //我的页面
+
+  currentPage(){
+
+  }
 
   @override
   Widget build(BuildContext context) {
-
+    return null;
   }
 }
