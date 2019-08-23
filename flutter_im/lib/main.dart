9bd1762e5922fc082375562eb675fdec09a59a1c
hcw <897796171@qq.com>
2019-08-23 10:17:01 +0800

    基础 组件

--- flutter_im/lib/main.dart
+++ flutter_im/lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import './app.dart';
+import './search.dart';
+import './loading.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "聊天室",
       theme: mDefaultTheme,
+      routes: <String, WidgetBuilder>{
+        "app": (BuildContext context) => new App(),
+        "/friends": (_) => new WebviewScaffold(
+              //Webview 插件
+              url: "https://flutter.io/",
+              appBar: new AppBar(
+                title: new Text("Flutter 官网"),
+              ),
+              withZoom: true,
+              withLocalStorage: true,
+            ),
+        "search": (BuildContext context) => new Search(),
+      },
+      home: new LoadingPage(),
     ));
 
 final ThemeData mDefaultTheme = new ThemeData(
