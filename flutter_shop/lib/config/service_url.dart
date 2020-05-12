5e4e8b371ac79b22aaa902a9aa13030b64470bc3
jerry <568384493@qq.com>
2020-05-12 22:51:40 +0800

    状态管理

--- flutter_shop/lib/config/service_url.dart
+++ flutter_shop/lib/config/service_url.dart
@@ -2,5 +2,5 @@ const serviceUrl = 'http://192.168.3.8:8000/';
 const servicePath = {
   'homePageContext': serviceUrl + 'wxmini/homePageContent/', // 商家首页信息
   'homePageBelowten': serviceUrl + 'wxmini/homePageBelowten/', // 商家首页火爆商品
-  'getCategory': serviceUrl + 'wxmini/getCategory', //商品类别信息
+  'getCategory': serviceUrl + 'wxmini/getCategory/', //商品类别信息
 };
--- flutter_shop/lib/pages/category_page.dart
+++ flutter_shop/lib/pages/category_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import '../models/category.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryPage extends StatefulWidget {
   // final Widget child;
@@ -12,14 +14,76 @@ class _CategoryPage extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      child: Center(
-        child: Text('分类页面'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNav createState() => _LeftCategoryNav();
+}
+
+class _LeftCategoryNav extends State<LeftCategoryNav> {
+  List list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCategory();
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black12, width: 1)),
+          color: Colors.white,
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
       ),
     );
   }
 
-  void _getCategory() async {
-    await getRequest("getCategory").then((data) {});
+  void _getCategory() {
+    getRequest('getCategory').then((data) {
+      CategoryModel category = CategoryModel.fromJson(data);
+      setState(() {
+        list = category.data;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getCategory();
+    return Container(
+        width: ScreenUtil().setWidth(180),
+        decoration: BoxDecoration(
+            border: Border(
+          right: BorderSide(width: 1, color: Colors.black12),
+        )),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return _leftInkWell(index);
+          },
+          itemCount: list.length,
+        ));
   }
 }
