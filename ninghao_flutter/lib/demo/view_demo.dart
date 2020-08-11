3aadb177c615cb0abae0e47c3f2ff6bd71cc3973
HCW <568384493@qq.com>
2020-08-11 14:52:58 +0800

    PageView.build 按需生成页面

--- ninghao_flutter/lib/demo/view_demo.dart
+++ ninghao_flutter/lib/demo/view_demo.dart
@@ -1,6 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:ninghao_flutter/model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                posts[index].author,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PageView.builder(
+      itemBuilder: _pageItemBuilder,
+      itemCount: posts.length,
+    );
+  }
+}
+
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
