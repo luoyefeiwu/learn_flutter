39237930c2673731e44d7f911030018f51014a8a
HCW <568384493@qq.com>
2020-08-19 14:22:20 +0800

    添加处理反馈

---
+++ mqsb/lib/violation/feedback.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//处理反馈
+class Feedback extends StatefulWidget {
+  @override
+  State createState() => _FeedbackState();
+}
+
+class _FeedbackState extends State<Feedback> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
