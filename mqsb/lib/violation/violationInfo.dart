d45f820c6489977cd503a6193977fdd860bf5c72
HCW <568384493@qq.com>
2020-08-19 14:04:22 +0800

    添加违规详情

---
+++ mqsb/lib/violation/violationInfo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//违规详情
+class ViolationInfo extends StatefulWidget{
+
+  @override
+  State createState() =>_violationInfoState();
+}
+
+class _violationInfoState extends State<ViolationInfo>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
